e5813fe25ef14e6481c4b8904897233e0461f0b0
JaloliddinGofurov <jaloliddingofurov.smh@gmail.com>
2025-05-09 18:02:15 +0500

    Add chage main img function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+const modifiers = {
+  imgThumbnailActive: 'img-showcase__thumbnail--active'
+};
+
+
+// SHOPPING CART
 const elSiteHeaderCartLink = document.querySelector('.js-site-header-cart-link');
 const elSiteHeaderCartModal = document.querySelector('.site-header__cart-modal');
 
@@ -8,3 +14,25 @@ if (elSiteHeaderCartLink) {
     elSiteHeaderCartModal.classList.toggle('site-header__cart-modal--open');
   });
 }
+
+// IMAGE SHOWCASE
+const elImgShowcaseActiveImg = document.querySelector('.img-showcase__active-img');
+const elsImgShowcaseThumbnailButton = document.querySelectorAll('.js-img-showcase-thumbnail-button');
+const elsImgThumbnail = document.querySelectorAll('.img-showcase__thumbnail');
+
+elsImgShowcaseThumbnailButton.forEach(function (elButton) {
+  elButton.addEventListener('click', function () {
+    // Remove
+    elsImgThumbnail.forEach(function (elImgThumbnail) {
+      elImgThumbnail.classList.remove(modifiers.imgThumbnailActive);
+    });
+
+    // Add clicked button
+    elButton.parentElement.classList.add(modifiers.imgThumbnailActive);
+
+    // Update active image src accordingly
+    elImgShowcaseActiveImg.src = elButton.dataset.imgShowcaseBig;
+    elImgShowcaseActiveImg.srcset = `${elButton.dataset.imgShowcaseBig} 1x, ${elButton.dataset.imgShowcaseRetine} 2x`;
+
+  });
+});
